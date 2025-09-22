bd1e468fec1ebc17bfb28e664ceb217cafb6595b
JAfifah <jamiatul.afifah2735@gmail.com>
2025-09-22 22:52:50 +0700

    remove false task

--- JS3/bin/prak3.dart
+++
@@ -1,7 +0,0 @@
-void main() {
-  for (int index = 10; index < 27; index++) {
-    if (index == 21) break;                // hentikan loop sepenuhnya saat index == 21
-    else if (index > 1 && index < 17) continue; // lewati iterasi untuk index < 17
-    print(index);
-  }
-}
